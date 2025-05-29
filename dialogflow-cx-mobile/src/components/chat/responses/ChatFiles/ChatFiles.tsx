ecc9239a0c27c7e3a44e06a19f42c9d57a84ee96
Gareth Marland <gareth.marland@gmail.com>
2025-05-29 23:34:29 +0100

    working on git

--- dialogflow-cx-mobile/src/components/chat/responses/ChatFiles/ChatFiles.tsx
+++ dialogflow-cx-mobile/src/components/chat/responses/ChatFiles/ChatFiles.tsx
@@ -1,46 +1,42 @@
 import React from 'react';
-import {View, Text, Image, TouchableOpacity, Linking} from 'react-native';
-import Svg, {Path} from 'react-native-svg';
+import { View, Text, Image, TouchableOpacity, Linking } from 'react-native';
+import Svg, { Path } from 'react-native-svg';
 
-import {styles} from './ChatFiles.styles';
-import {IFile} from '../../../interfaces/IFile';
+import type { IFile } from '../../../../interfaces/responses/IFile';
+import { styles } from './ChatFiles.styles';
 
-const ChatFiles = ({files}: {files: IFile[]}) => {
-  const renderCard = (iFile: IFile, index: number) => {
-    return (
-      <View key={index} style={styles.containerStyles}>
-        {iFile.image && iFile.image.length > 0 && (
-          <Image source={{uri: iFile.image}} style={styles.image} />
-        )}
-        {iFile.name && iFile.name.length > 0 && (
-          <Text style={styles.title}>{iFile.name}</Text>
-        )}
-        <Svg width={20} height={20} viewBox="0 0 24 24">
-          <Path d="M5 20h14v-2H5zM19 9h-4V3H9v6H5l7 7z"></Path>
-        </Svg>
-      </View>
+interface ChatFilesProps {
+  files: IFile[];
+}
+
+const ChatFiles: React.FC<ChatFilesProps> = ({ files }) => {
+  const renderCard = (file: IFile) => (
+    <View style={styles.containerStyles}>
+      {file.image && (
+        <Image source={{ uri: file.image }} style={styles.image} />
+      )}
+      {file.name && <Text style={styles.title}>{file.name}</Text>}
+      <Svg width={20} height={20} viewBox="0 0 24 24">
+        <Path d="M5 20h14v-2H5zM19 9h-4V3H9v6H5l7 7z" />
+      </Svg>
+    </View>
+  );
+
+  const renderFile = (file: IFile, index: number) => {
+    const key = `file_${index}`;
+
+    return file.url ? (
+      <TouchableOpacity key={key} onPress={() => Linking.openURL(file.url)}>
+        {renderCard(file)}
+      </TouchableOpacity>
+    ) : (
+      <View key={key}>{renderCard(file)}</View>
     );
-  };
-
-  const renderFile = (iFile: IFile, index: number) => {
-    if (iFile.url && iFile.url.length > 0) {
-      return (
-        <TouchableOpacity
-          key={`touch_${index}`}
-          onPress={() => Linking.openURL(iFile.url)}>
-          {renderCard(iFile, index)}
-        </TouchableOpacity>
-      );
-    } else {
-      return renderCard(iFile, index);
-    }
   };
 
   return (
     <View style={styles.chipsContainer}>
-      {files &&
-        files.length > 0 &&
-        files.map((f, index) => renderFile(f, index))}
+      {files?.map((file, index) => renderFile(file, index))}
     </View>
   );
 };
